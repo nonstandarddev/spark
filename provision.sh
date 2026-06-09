21cbe3410e9f87981cffd26eb1886c86d7228349
nonstandarddev <nonstandarddev@gmail.com>
2026-06-09 13:35:28 +0100

    Improve error message for unrecognized mechanism

--- provision.sh
+++ provision.sh
@@ -3,14 +3,11 @@
 SPARK_MECHANISM=$1
 
 if [[ "$SPARK_MECHANISM" == "master" ]]; then
-  echo "Provisioning master service..."
   start-master.sh -h $SPARK_MASTER_HOST -p $SPARK_MASTER_PORT
 elif [[ "$SPARK_MECHANISM" == "worker" ]]; then
-  echo "Provisioning worker service..."
   start-worker.sh $SPARK_MASTER_URL
 elif [[ "$SPARK_MECHANISM" == "history" ]]; then
-  echo "Provisioning history service..."
   start-history-server.sh
 else
-   echo "Unrecognised provisioning mechanism!"
+   echo "Provided mechanism '$SPARK_MECHANISM' not recognised (must be one of: 'master', 'worker' or 'history')"
 fi
