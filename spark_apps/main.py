1924c1acc403e91c99a79071ae59b4c6a023a64f
nonstandarddev <nonstandarddev@gmail.com>
2026-06-11 00:08:23 +0100

    Added module-level explanation

--- spark_apps/main.py
+++ spark_apps/main.py
@@ -1,7 +1,18 @@
+"""
+Outlines the __general__ approach to building Apache Spark applications.
+
+* Cluster configuration details (via `SparkConf` & `SparkContext`)
+* Data ingestion
+* Execution plan
+* Execution trigger (i.e. the trigger for Apache Spark to process)
+"""
+
 from pyspark import (
     SparkConf,
     SparkContext
 )
+
+# ---- 1: Configuration ----
 
 conf = (
     SparkConf()
@@ -9,13 +20,19 @@ conf = (
 )
 sc = SparkContext(conf=conf)
 
+# ---- 2: Data Assembly ----
+
 rdd = sc.parallelize(range(100))
+
+# ---- 3: Execution Plan (Lazy Evaluation) ----
 
 plan = (
     rdd
     .map(lambda x: x * x)
     .filter(lambda x: x % 2 == 0)
 )
+
+# ---- 4: Execution Trigger ----
 
 result = plan.collect()
 
